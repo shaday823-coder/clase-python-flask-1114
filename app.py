f25708d18842f8e0ef8f2234d45795cf50e65d6f
shaday823-coder <zcarrillor823@inemkennedy.edu.co>
2026-05-15 13:07:33 -0500

    TAREA #2

--- app.py
+++ app.py
@@ -15,6 +15,22 @@ def inicio():
     return render_template("index.html")
 
 
+@app.route("/HOLA")
+def hola():
+    # `render_template` busca archivos dentro de la carpeta `templates`.
+    return render_template("HOLA.html")
+
+@app.route("/ADIOS")
+def adios():
+    # `render_template` busca archivos dentro de la carpeta `templates`.
+    return render_template("ADIOS.html")
+
+@app.route("/LOVE")
+def love():
+    # `render_template` busca archivos dentro de la carpeta `templates`.
+    return render_template("LOVE.html")
+
+
 # Este bloque se ejecuta solo si corremos `python app.py` desde la terminal.
 if __name__ == "__main__":
     # `debug=True` sirve en desarrollo porque reinicia el servidor
